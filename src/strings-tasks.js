efb1bcb05f521fd143be247611c46dfee184dc2d
v4to <andrew.egorcev@gmail.com>
2025-10-27 22:23:56 +0200

    feat: implement findLongestWord function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -380,8 +380,18 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const words = sentence.split(' ');
+
+  let longestWord = '';
+
+  for (let i = 0; i < words.length; i += 1) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
 }
 
 /**
