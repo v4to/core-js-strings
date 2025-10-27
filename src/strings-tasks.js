22a0dfeb0d7bc5f8f339b426b896d035794d7f2b
v4to <andrew.egorcev@gmail.com>
2025-10-27 15:38:28 +0200

    feat: implement repeatString function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -133,8 +133,12 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times > 0) {
+    return str.repeat(times);
+  }
+
+  return '';
 }
 
 /**
