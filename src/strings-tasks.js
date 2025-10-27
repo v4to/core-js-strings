c782560af43ae7dcad2542d7f8b626e23636afb9
v4to <andrew.egorcev@gmail.com>
2025-10-27 22:28:35 +0200

    feat: implement reverseWords function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -404,8 +404,13 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map((word) => {
+      return word.split('').reverse().join('');
+    })
+    .join(' ');
 }
 
 /**
