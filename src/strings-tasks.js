08e670ee082039564d70c116ce70a342080b235a
v4to <andrew.egorcev@gmail.com>
2025-10-27 15:26:20 +0200

    feat: implement removeTrailingWhitespaces function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -116,8 +116,8 @@ function removeLeadingWhitespaces(value) {
  *   removeTrailingWhitespaces('cat ') => 'cat'
  *   removeTrailingWhitespaces('\t\t\tHello, World! ') => '\t\t\tHello, World!'
  */
-function removeTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeTrailingWhitespaces(value) {
+  return value.trimEnd();
 }
 
 /**
